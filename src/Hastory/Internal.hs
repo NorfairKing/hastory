f0ea3e243bc114cada214c75a6c9bc2d4fd5f19a
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-04-18 11:38:46 +0200

    Removed the iostring dependency

--- src/Hastory/Internal.hs
+++ src/Hastory/Internal.hs
@@ -7,6 +7,7 @@ import Import
 
 import qualified Data.Aeson as JSON
 import qualified Data.ByteString.Lazy.Char8 as LB8
+import qualified Data.ByteString.Lazy as LB
 
 import Hastory.Types
 
@@ -15,15 +16,13 @@ hastoryDir = do
     home <- getHomeDir
     pure $ home </> $(mkRelDir ".hastory")
 
-
 histfile :: IO (Path Abs File)
-histfile =
-    fmap (</> $(mkRelFile "commandhistory.log")) hastoryDir
+histfile = fmap (</> $(mkRelFile "commandhistory.log")) hastoryDir
 
 getHistory :: IO [Entry]
 getHistory = do
     hFile <- histfile
-    contents <- readFile hFile
+    contents <- LB.readFile $ toFilePath hFile
     let encodedEntries = LB8.lines contents
     let entries = catMaybes $ map JSON.decode encodedEntries
     pure entries
--- src/Import.hs
+++ src/Import.hs
@@ -8,7 +8,6 @@ import Data.Maybe as X
 import Data.Monoid as X
 import GHC.Generics as X
 import Prelude as X
-       hiding (writeFile, readFile, putStrLn, putStr, appendFile)
 import Safe as X
 import System.Exit as X
 
@@ -18,5 +17,3 @@ import Data.Validity.Text as X ()
 
 import Path as X
 import Path.IO as X
-
-import Data.IOString as X
