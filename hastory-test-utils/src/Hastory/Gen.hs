85e0bd1ed214315fbfcdd4a3009a95408dd15b9e
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-05-24 04:13:15 +0200

    Major refactor

--- hastory-test-utils/src/Hastory/Gen.hs
+++ hastory-test-utils/src/Hastory/Gen.hs
@@ -6,10 +6,7 @@ import TestImport
 
 import Hastory.Types
 
-instance GenUnchecked Entry where
-    genUnchecked =
-        Entry <$> genUnchecked <*> genUnchecked <*> genUnchecked <*>
-        genUnchecked <*> genUnchecked
+instance GenUnchecked Entry
 
 instance GenValid Entry where
     genValid = Entry <$> genValid <*> genValid <*> genValid <*> genValid <*> genValid
