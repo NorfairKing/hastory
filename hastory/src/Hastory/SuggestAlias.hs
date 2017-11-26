bf7cd1407bc46417cabb01b1fa6f2681ea6b9c8d
Joey Bloom <15joeybloom@gmail.com>
2017-11-26 11:30:48 -0600

    Suggest aliases that the user might want to create.
    Count the frequency of commands the user has issued in the last 7 days.
    
    There is significant code duplication between Suggest.hs and Recent.hs; further
    refactoring needed.

--- hastory/src/Hastory/SuggestAlias.hs
+++ hastory/src/Hastory/SuggestAlias.hs
@@ -4,7 +4,44 @@ module Hastory.SuggestAlias where
 
 import Import
 
+import Control.Arrow (second)
+import Data.HashMap.Lazy (HashMap)
+import qualified Data.HashMap.Lazy as HM
+import Data.Hashable (Hashable)
+import qualified Data.Text as T
+import Data.Text (Text)
+
+import Hastory.Internal
 import Hastory.OptParse.Types
+import Hastory.Types
 
 suggest :: (MonadIO m, MonadThrow m, MonadReader Settings m) => m ()
-suggest = liftIO $ die "Suggest Alias here"
+suggest = do
+    tups <- suggestions
+    let maxlen = maximum $ map (length . show . snd) tups
+        formatTup (t, x) =
+            show x ++
+            replicate (maxlen - length (show x) + 1) ' ' ++ T.unpack (T.strip t)
+    liftIO $ forM_ tups $ putStrLn . formatTup
+
+suggestions ::
+       (MonadIO m, MonadThrow m, MonadReader Settings m) => m [(Text, Integer)]
+suggestions = do
+    rawEnts <- getLastNDaysOfHistory 7
+    let entries = filter (not . T.isPrefixOf (T.pack "cd ") . entryText) rawEnts
+    let counts = doCountsWith entryText (const 1.0) entries
+    let tups = reverse $ sortOn snd $ HM.toList counts
+    return $ take 10 $ map (second round) tups
+  where
+    doCountsWith ::
+           (Eq b, Hashable b)
+        => (a -> b)
+        -> (a -> Double)
+        -> [a]
+        -> HashMap b Double
+    doCountsWith conv func = foldl go HM.empty
+      where
+        go hm k = HM.alter a (conv k) hm
+          where
+            a Nothing = Just 0
+            a (Just d) = Just $ d + func k
