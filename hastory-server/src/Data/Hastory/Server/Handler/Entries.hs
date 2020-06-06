258a829363a80fb21ca4504664f001794c3af446
Steven Leiva <leiva.steven@gmail.com>
2020-06-05 23:08:12 -0500

    Delete unnecessary comments.

--- hastory-server/src/Data/Hastory/Server/Handler/Entries.hs
+++ hastory-server/src/Data/Hastory/Server/Handler/Entries.hs
@@ -12,14 +12,3 @@ createEntryHandler ServerSettings {..} cookie syncReq = do
       uniqueContentHash = UniqueContentHash (serverEntryContentHash serverEntry)
   _ <- runDB upsertIfNotExist _ssDbPool
   pure NoContent
--- createEntryHandler :: ServerSettings -> AuthResult AuthCookie -> SyncRequest -> Handler NoContent
--- createEntryHandler _ BadPassword _ = unAuthenticated
--- createEntryHandler _ Indefinite _ = unAuthenticated
--- createEntryHandler _ NoSuchUser _ = unAuthenticated
--- createEntryHandler ServerSettings {..} (Authenticated cookie) syncReq = do
---   user <- runDB (getBy $ UniqueUsername (unAuthCookie cookie)) _ssDbPool >>= ensureWith err401
---   let serverEntry = toServerEntry syncReq (entityKey user)
---       upsertIfNotExist = upsertBy uniqueContentHash serverEntry []
---       uniqueContentHash = UniqueContentHash (serverEntryContentHash serverEntry)
---   _ <- runDB upsertIfNotExist _ssDbPool
---   pure NoContent
