6051b3eca1c8883e38e7f6812256985f82525555
Steven Leiva <leiva.steven@gmail.com>
2020-06-19 09:02:19 -0500

    Add "extra deps" to nix package set.

--- nix/nixpkgs-version.nix
+++ nix/nixpkgs-version.nix
@@ -1,7 +1,4 @@
-# To update:
-# nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/07723f14bb132f6d65c74b953bef39cd80457273.tar.gz
-
 {
-  rev = "e6b93257b0ba070bd12810eeacbe281396c24316";
-  sha256 = "0cv7hv86cjzrjamgz5mgy4n5ycisglzg89v3b3938a2bkkh8rwzi";
+  rev = "a87b506140a7267477103759c3f8da5b2e8d994e";
+  sha256 = "sha256:0h8i0b2dyz8bppkfs5srkaq4hrc9ngb6idg7cq9ngvz3szr6xx08";
 }
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -35,7 +35,32 @@ with final.haskell.lib;
             _:
               {})
             ) (
-              self: super: final.hastoryPackages
+              self: super:
+                let
+                  # Passwords
+                  passwordRepo =
+                    final.fetchFromGitHub {
+                      owner = "cdepillabout";
+                      repo = "password";
+                      rev = "26434d4f6888faf8dc36425b20b59f0b5056d7f5";
+                      sha256 =
+                        "sha256:0kbrw7zcn687h61h574z5k8p7z671whblcrmd6q21gsa2pyrk4ll";
+                    };
+                  passwordPkg =
+                    name:
+                      dontCheck (
+                        self.callCabal2nix name ( passwordRepo + "/${name}" ) {}
+                      );
+                  passwordPackages =
+                    final.lib.genAttrs [
+                      "password"
+                      "password-instances"
+                    ] passwordPkg;
+                in
+                  final.hastoryPackages // {
+                    # Passwords
+                    ghc-byteorder = self.callHackage "ghc-byteorder" "4.11.0.0.10" {};
+                  } // passwordPackages
             );
         }
     );
