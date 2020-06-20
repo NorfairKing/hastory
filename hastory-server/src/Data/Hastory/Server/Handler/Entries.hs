edd5aee49560398b95fde905d9d63d1ad85eee48
Steven Leiva <leiva.steven@gmail.com>
2020-06-20 09:06:09 -0500

    Reorder let declarations in Entries.hs
    This increasing legibility of code, since it can now be read top to
    bottom.

--- hastory-server/src/Data/Hastory/Server/Handler/Entries.hs
+++ hastory-server/src/Data/Hastory/Server/Handler/Entries.hs
@@ -5,8 +5,8 @@ import Data.Hastory.Server.Handler.Import
 createEntryHandler :: AuthCookie -> SyncRequest -> HastoryHandler NoContent
 createEntryHandler cookie syncReq = do
   user <- (runDB . getBy $ UniqueUsername (unAuthCookie cookie)) >>= ensureWith err401
-  let serverEntry = toServerEntry syncReq (entityKey user)
-      upsertIfNotExist = upsertBy uniqueContentHash serverEntry []
+  let upsertIfNotExist = upsertBy uniqueContentHash serverEntry []
       uniqueContentHash = UniqueContentHash (serverEntryContentHash serverEntry)
+      serverEntry = toServerEntry syncReq (entityKey user)
   _ <- runDB upsertIfNotExist
   pure NoContent
