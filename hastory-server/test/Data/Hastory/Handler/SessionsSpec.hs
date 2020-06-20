ed620487c6b51ea09d0b2838391a269fb0d9ca08
Steven Leiva <leiva.steven@gmail.com>
2020-06-20 09:24:31 -0500

    Convert "POST / sessions" unit test to prop test.

--- hastory-server/test/Data/Hastory/Handler/SessionsSpec.hs
+++ hastory-server/test/Data/Hastory/Handler/SessionsSpec.hs
@@ -6,8 +6,10 @@ module Data.Hastory.Handler.SessionsSpec
   ) where
 
 import Data.Either
+import Data.Hastory.Gen ()
 import Servant.Client
 import Test.Hspec
+import Test.Validity
 
 import Data.Hastory.Server.TestUtils
 
@@ -16,12 +18,13 @@ spec =
   serverSpec $
   describe "POST /sessions" $ do
     context "incorrect login" $
-      it "is a 401" $ \ServerInfo {..} -> do
-        let userForm = UserForm (Username "Paul") "Passw0rd"
-        Right _ <- createUser siClientEnv userForm
-        let incorrectPasswordForm = UserForm (Username "Paul") "baddPassw0rd"
-        Left (FailureResponse _requestF resp) <- loginUser siClientEnv incorrectPasswordForm
-        responseStatusCode resp `shouldBe` status401
+      it "is a 401" $ \ServerInfo {..} ->
+        forAllValid $ \(username, password) -> do
+          let userForm = UserForm username password
+          Right _ <- createUser siClientEnv userForm
+          let incorrectPasswordForm = UserForm username (password <> "badsuffix")
+          Left (FailureResponse _requestF resp) <- loginUser siClientEnv incorrectPasswordForm
+          responseStatusCode resp `shouldBe` status401
     context "correct login" $
       it "returns a cookie" $ \ServerInfo {..} -> do
         let userForm = UserForm (Username "Paul") "Passw0rd"
