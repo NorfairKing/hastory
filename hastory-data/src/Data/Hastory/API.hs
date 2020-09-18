9dd690e538e1f45551a62b2e4e4dac0c557a9e36
Steven Leiva <leiva.steven@gmail.com>
2020-09-18 15:29:10 -0500

    Correct return type of "/entries".

--- hastory-data/src/Data/Hastory/API.hs
+++ hastory-data/src/Data/Hastory/API.hs
@@ -21,8 +21,7 @@ import Data.Hastory.Types
 
 type AuthCookies = '[ Header "Set-Cookie" Text]
 
-type EntriesAPI
-   = "entries" :> ReqBody '[ JSON] SyncRequest :> PostCreated '[ JSON] [Key ServerEntry]
+type EntriesAPI = "entries" :> ReqBody '[ JSON] SyncRequest :> PostCreated '[ JSON] NoContent
 
 type ProtectedAPI = Auth '[ JWT] AuthCookie
 
@@ -83,7 +82,7 @@ extractJWTCookie headersList =
 -- See https://hackage.haskell.org/package/servant-client-0.16.0.1/docs/Servant-Client.html#v:client
 createUserClient :: UserForm -> ClientM UserId
 createSessionClient :: UserForm -> ClientM (Headers AuthCookies NoContent)
-createEntryClient :: Token -> SyncRequest -> ClientM [Key ServerEntry]
+createEntryClient :: Token -> SyncRequest -> ClientM NoContent
 (createUserClient :<|> createSessionClient :<|> createEntryClient) = client api
 
 -- | Re-export of runClientM
