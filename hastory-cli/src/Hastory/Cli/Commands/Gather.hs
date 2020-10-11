ccadac8ceb6bac73a5e690f519d9b20a3a6decfd
Steven Leiva <leiva.steven@gmail.com>
2020-10-11 17:03:20 -0500

    Remove `MonadThrow` from `gather` command

--- hastory-cli/src/Hastory/Cli/Commands/Gather.hs
+++ hastory-cli/src/Hastory/Cli/Commands/Gather.hs
@@ -3,7 +3,6 @@
 
 module Hastory.Cli.Commands.Gather where
 
-import Control.Monad.Catch
 import Control.Monad.IO.Unlift (MonadUnliftIO)
 import Control.Monad.Reader
 import Data.Text (Text)
@@ -15,18 +14,18 @@ import Hastory.Cli.Internal
 import Hastory.Cli.OptParse.Types
 import Hastory.Data.Client.DB
 
-gather :: (MonadReader Settings m, MonadThrow m, MonadUnliftIO m) => m (Entity Entry)
+gather :: (MonadReader Settings m, MonadUnliftIO m) => m (Entity Entry)
 gather = do
   text <- liftIO T.getContents
   gatherFrom text
 
-gatherFrom :: (MonadReader Settings m, MonadThrow m, MonadUnliftIO m) => Text -> m (Entity Entry)
+gatherFrom :: (MonadReader Settings m, MonadUnliftIO m) => Text -> m (Entity Entry)
 gatherFrom text = do
   entry <- liftIO $ gatherEntryWith text
   storeHistory entry
 
-storeHistory :: (MonadReader Settings m, MonadThrow m, MonadUnliftIO m) => Entry -> m (Entity Entry)
-storeHistory entry@Entry {..} = runDb (upsertBy uniqueRecord entry noUpdate)
+storeHistory :: (MonadReader Settings m, MonadUnliftIO m) => Entry -> m (Entity Entry)
+storeHistory entry@Entry {..} = runDb' (upsertBy uniqueRecord entry noUpdate)
   where
     uniqueRecord = EntryData entryText entryWorkingDir entryDateTime entryUser
     noUpdate = []
