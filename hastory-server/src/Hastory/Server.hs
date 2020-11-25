d655967a4c693134817827c8770783881e3137a6
Steven Leiva <leiva.steven@gmail.com>
2020-11-25 09:13:03 -0600

    Add configuration parsing.
    We used
    [template-optparse](https://github.com/norfairking/template-optparse) to
    guide our implementation as to best practices.

--- hastory-server/src/Hastory/Server.hs
+++ hastory-server/src/Hastory/Server.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -8,7 +9,7 @@ module Hastory.Server where
 
 import Conduit (MonadUnliftIO)
 import Control.Monad
-import Control.Monad.Logger (MonadLogger)
+import Control.Monad.Logger (MonadLogger, runStdoutLoggingT)
 import Control.Monad.Logger.CallStack (logInfo)
 import Crypto.JOSE.JWK
 import Data.Maybe
@@ -17,37 +18,14 @@ import qualified Database.Persist.Sqlite as SQL
 import Hastory.API
 import Hastory.Data.Server.DB (migrateAll)
 import Hastory.Server.Handler
-import Lens.Micro
+import Hastory.Server.OptParse
+import Lens.Micro hiding (sets)
 import qualified Network.HTTP.Types as HTTP
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
-import qualified Options.Applicative as A
 import Path.IO
 import Servant.Auth.Server
 import Prelude
-
-data Options
-  = Options
-      { -- | Port that will be used by the server; defaults to 8080.
-        optPort :: Int,
-        -- | If provided, server will log to this file. If not provided, server
-        -- will log to "server.logs".
-        optLogFile :: Maybe FilePath,
-        -- | If provided, server will read / write JWK key to this file. Default is
-        -- "hastory.key".
-        optKeyFile :: Maybe FilePath
-      }
-  deriving (Show, Eq)
-
--- | Parser for the command line flags of Hastory Server.
-optParser :: A.ParserInfo Options
-optParser =
-  A.info
-    ( Options <$> A.option A.auto (A.value 8080 <> A.showDefault <> A.long "port" <> A.short 'p')
-        <*> A.option A.auto (A.value (Just "server.logs") <> A.long "log-output" <> A.short 'l')
-        <*> A.option A.auto (A.value (Just "hastory.key") <> A.long "hastory-key" <> A.short 'k')
-    )
-    mempty
 
 -- | Main server logic for Hastory Server.
 server :: ServerSettings -> Server HastoryAPI
@@ -71,35 +49,42 @@ mkWarpLogger :: FilePath -> Wai.Request -> HTTP.Status -> Maybe Integer -> IO ()
 mkWarpLogger logPath req _ _ = appendFile logPath $ show req <> "\n"
 
 -- | Warp server settings.
-mkWarpSettings :: Options -> Warp.Settings
-mkWarpSettings Options {..} =
+mkWarpSettings :: ServeSettings -> Warp.Settings
+mkWarpSettings ServeSettings {..} =
   Warp.setTimeout 20
-    $ Warp.setPort optPort
-    $ maybe id (Warp.setLogger . mkWarpLogger) optLogFile Warp.defaultSettings
+    $ Warp.setPort serveSettingsPort
+    $ Warp.setLogger (mkWarpLogger $ toFilePath serveSettingsLogFile) Warp.defaultSettings
 
 -- | Displays the port this server will use. This port is configurable via command-line flags.
-reportPort :: MonadLogger m => Options -> m ()
-reportPort Options {..} = logInfo $ "Starting server on port " <> T.pack (show optPort)
+reportPort :: MonadLogger m => Int -> m ()
+reportPort port = logInfo $ "Starting server on port " <> T.pack (show port)
 
--- | Starts a webserver by reading command line flags and the HASTORY_SERVER_JWK
--- environmental variable.
-hastoryServer :: (MonadIO m, MonadLogger m, MonadUnliftIO m) => m ()
+-- | Runs command by reading from the command line, environment, and config file.
+hastoryServer :: IO ()
 hastoryServer = do
-  options@Options {..} <- liftIO $ A.execParser optParser
-  keyFile <- resolveFile' (fromMaybe "hastory.key" optKeyFile)
-  signingKey <- liftIO (getSigningKey keyFile)
-  serverSetPwDifficulty <- liftIO (passwordDifficultyOrExit 10)
-  let serverSetCookieSettings = defaultCookieSettings
-      serverSetJWTSettings = defaultJWTSettings signingKey
-  reportPort options
-  dbFile <- resolveFile' "hastory.sqlite3"
-  ensureDir (parent dbFile)
-  SQL.withSqlitePoolInfo
-    (SQL.mkSqliteConnectionInfo (T.pack $ fromAbsFile dbFile) & SQL.fkEnabled .~ False)
-    1
-    $ \serverSetPool -> do
-      void $ SQL.runSqlPool (SQL.runMigrationSilent migrateAll) serverSetPool
-      liftIO $ Warp.runSettings (mkWarpSettings options) (app ServerSettings {..})
+  Instructions d sets <- getInstructions
+  runReaderT (dispatch d) sets
+
+dispatch :: MonadUnliftIO m => Dispatch -> m ()
+dispatch (DispatchServe serveSets) = hastoryServe serveSets
+
+-- | Starts a webserver by reading command line flags
+hastoryServe :: MonadUnliftIO m => ServeSettings -> m ()
+hastoryServe serveSettings@ServeSettings {..} =
+  runStdoutLoggingT $ do
+    signingKey <- liftIO (getSigningKey serveSettingsKeyFile)
+    serverSetPwDifficulty <- liftIO (passwordDifficultyOrExit 10)
+    let serverSetCookieSettings = defaultCookieSettings
+        serverSetJWTSettings = defaultJWTSettings signingKey
+    reportPort serveSettingsPort
+    dbFile <- resolveFile' "hastory.sqlite3"
+    ensureDir (parent dbFile)
+    SQL.withSqlitePoolInfo
+      (SQL.mkSqliteConnectionInfo (T.pack $ fromAbsFile dbFile) & SQL.fkEnabled .~ False)
+      1
+      $ \serverSetPool -> do
+        void $ SQL.runSqlPool (SQL.runMigrationSilent migrateAll) serverSetPool
+        liftIO $ Warp.runSettings (mkWarpSettings serveSettings) (app ServerSettings {..})
 
 -- | Reads the signing key from the given file. If the file does not exist, then
 -- the file, with a JWK, will be created and read from.
---
+++ hastory-server/src/Hastory/Server/OptParse.hs
@@ -0,0 +1,155 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Hastory.Server.OptParse where
+
+import Control.Monad
+import Data.Maybe
+import qualified Data.Text as T
+import Data.Yaml
+import qualified Env
+import GHC.Generics (Generic)
+import Options.Applicative as OptParse
+import qualified Options.Applicative.Help as OptParse (string)
+import Path
+import Path.IO
+import Text.Read
+import YamlParse.Applicative as YamlParse
+
+data Instructions = Instructions Dispatch Settings deriving (Show, Eq, Generic)
+
+newtype Dispatch = DispatchServe ServeSettings deriving (Show, Eq, Generic)
+
+data Settings = Settings deriving (Show, Eq, Generic)
+
+data ServeSettings = ServeSettings {serveSettingsPort :: Int, serveSettingsLogFile :: Path Abs File, serveSettingsKeyFile :: Path Abs File} deriving (Show, Eq, Generic)
+
+data Arguments = Arguments Command Flags deriving (Show, Eq, Generic)
+
+newtype Command = CommandServe ServeArgs deriving (Show, Eq, Generic)
+
+data ServeArgs = ServeArgs {serveArgsPort :: Maybe Int, serveArgsLogFile :: Maybe FilePath, serveArgsKeyFile :: Maybe FilePath} deriving (Show, Eq, Generic)
+
+newtype Flags = Flags {flagsConfigFile :: Maybe FilePath} deriving (Show, Eq, Generic)
+
+data Environment = Environment {envPort :: Maybe Int, envLogFile :: Maybe FilePath, envKeyFile :: Maybe FilePath, envConfigFile :: Maybe FilePath} deriving (Show, Eq, Generic)
+
+data Configuration = Configuration {configPort :: Maybe Int, configLogFile :: Maybe FilePath, configKeyFile :: Maybe FilePath} deriving (Show, Eq, Generic)
+
+instance FromJSON Configuration where
+  parseJSON = viaYamlSchema
+
+instance YamlSchema Configuration where
+  yamlSchema = objectParser "Configuration" $ Configuration <$> optionalField "port" "Port to start server on." <*> optionalField "log" "File to save logs to." <*> optionalField "key" "File to read / write JWK key from / to."
+
+getInstructions :: IO Instructions
+getInstructions = do
+  args@(Arguments _ flags) <- getArguments
+  env <- getEnvironment
+  config <- getConfiguration flags env
+  combineToInstructions args env config
+
+getArguments :: IO Arguments
+getArguments = customExecParser prefs_ argParser
+
+getEnvironment :: IO Environment
+getEnvironment = Env.parse (Env.header "Environment") environmentParser
+
+combineToInstructions :: Arguments -> Environment -> Maybe Configuration -> IO Instructions
+combineToInstructions (Arguments cmd _flags) Environment {..} mConf = Instructions <$> dispatch <*> settings
+  where
+    dispatch =
+      case cmd of
+        CommandServe ServeArgs {..} -> do
+          let serveSettingsPort = fromMaybe 8080 (serveArgsPort <|> envPort <|> mc configPort)
+          serveSettingsLogFile <-
+            maybe defaultLogFile resolveFile' (serveArgsLogFile <|> envLogFile <|> mc configLogFile)
+          serveSettingsKeyFile <-
+            maybe defaultKeyFile resolveFile' (serveArgsKeyFile <|> envKeyFile <|> mc configKeyFile)
+          pure $ DispatchServe (ServeSettings {..})
+    settings = pure Settings
+    mc f = mConf >>= f
+
+defaultKeyFile :: IO (Path Abs File)
+defaultKeyFile = do
+  xdgConfigDir <- getXdgDir XdgData (Just [reldir|hastory-server|])
+  resolveFile xdgConfigDir "hastory.key"
+
+defaultLogFile :: IO (Path Abs File)
+defaultLogFile = do
+  xdgConfigDir <- getXdgDir XdgData (Just [reldir|hastory-server|])
+  resolveFile xdgConfigDir "hastory.logs"
+
+defaultConfigFile :: IO (Path Abs File)
+defaultConfigFile = do
+  xdgConfigDir <- getXdgDir XdgConfig (Just [reldir|hastory-server|])
+  resolveFile xdgConfigDir "config.yaml"
+
+getConfiguration :: Flags -> Environment -> IO (Maybe Configuration)
+getConfiguration Flags {..} Environment {..} =
+  case flagsConfigFile <|> envConfigFile of
+    Nothing -> defaultConfigFile >>= YamlParse.readConfigFile
+    Just cf -> resolveFile' cf >>= YamlParse.readConfigFile
+
+prefs_ :: OptParse.ParserPrefs
+prefs_ = OptParse.defaultPrefs {OptParse.prefShowHelpOnError = True, OptParse.prefShowHelpOnEmpty = True}
+
+argParser :: OptParse.ParserInfo Arguments
+argParser =
+  OptParse.info
+    (OptParse.helper <*> parseArgs)
+    (OptParse.fullDesc <> OptParse.footerDoc (Just $ OptParse.string footerStr))
+  where
+    footerStr =
+      unlines
+        [ Env.helpDoc environmentParser,
+          "",
+          "Configuration file format:",
+          T.unpack (YamlParse.prettySchemaDoc @Configuration)
+        ]
+
+parseArgs :: OptParse.Parser Arguments
+parseArgs = Arguments <$> parseCommand <*> parseFlags
+
+parseCommand :: OptParse.Parser Command
+parseCommand =
+  OptParse.hsubparser $
+    mconcat
+      [ OptParse.command "serve" $ CommandServe <$> parseCommandServe
+      ]
+
+parseCommandServe :: OptParse.ParserInfo ServeArgs
+parseCommandServe =
+  OptParse.info
+    (OptParse.helper <*> parseServeArgs)
+    (OptParse.fullDesc <> OptParse.progDesc "Start the server.")
+
+parseServeArgs :: OptParse.Parser ServeArgs
+parseServeArgs = ServeArgs <$> parsePort <*> parseLogFile <*> parseKeyFile
+  where
+    parsePort = optional $ option auto (short 'p' <> long "port" <> metavar "PORT" <> help "The port at which the server will be available.")
+    parseLogFile = optional $ strOption (short 'l' <> long "log" <> metavar "LOG" <> help "The file to which server logs will be written.")
+    parseKeyFile = optional $ strOption (short 'k' <> long "key" <> metavar "KEY" <> help "The file to which server keys will be read from / written to.")
+
+parseFlags :: OptParse.Parser Flags
+parseFlags = Flags <$> configFileParser
+  where
+    configFileParser :: OptParse.Parser (Maybe FilePath)
+    configFileParser = optional $ strOption (short 'c' <> long "config-file" <> metavar "FILEPATH" <> help "Path to an alternative config file.")
+
+environmentParser :: Env.Parser Env.Error Environment
+environmentParser =
+  Env.prefixed "HASTORY_SERVER_" $
+    Environment
+      <$> Env.var readNum "PORT" (Env.help "The port at which the server will be available." <> Env.def Nothing)
+      <*> Env.var (pure . Just <=< Env.nonempty) "LOG" (Env.help "The file to which server logs will be written." <> Env.def Nothing)
+      <*> Env.var (pure . Just <=< Env.nonempty) "KEY" (Env.help "The file to which server keys will be read from / written to." <> Env.def Nothing)
+      <*> Env.var (pure . Just <=< Env.nonempty) "CONFIG_FILE" (Env.help "Path to an alternative config file." <> Env.def Nothing)
+  where
+    readNum :: String -> Either Env.Error (Maybe Int)
+    readNum s = case readMaybe s of
+      Nothing -> Left $ Env.UnreadError (unwords ["Could not parse '", s, "' into a number"])
+      Just i -> Right (Just i)
